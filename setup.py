209a2660ae844b4241a8a00e0d89c56d07b22c57
yureehh <fabbri.juri@gmail.com>
2023-05-23 18:36:21 +0200

    linter

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='This repo gets cloned everytime I start a new Data Science project. A logical, reasonably standardized, but flexible project structure for doing and sharing data science work. See https://drivendata.github.io/cookiecutter-data-science/',
-    author='Yureeh',
-    license='MIT',
+    version="0.1.0",
+    description="This repo gets cloned everytime I start a new Data Science project. A logical, reasonably standardized, but flexible project structure for doing and sharing data science work. See https://drivendata.github.io/cookiecutter-data-science/",
+    author="Yureeh",
+    license="MIT",
 )
--- tests/test_environment.py
+++ tests/test_environment.py
@@ -3,9 +3,10 @@ import pytest
 
 REQUIRED_PYTHON = "python3"
 
+
 def test_python_version():
     system_major = sys.version_info.major
-# sourcery skip: no-conditionals-in-tests
+    # sourcery skip: no-conditionals-in-tests
     if REQUIRED_PYTHON == "python":
         required_major = 2
     elif REQUIRED_PYTHON == "python3":
@@ -13,4 +14,6 @@ def test_python_version():
     else:
         raise ValueError(f"Unrecognized python interpreter: {REQUIRED_PYTHON}")
 
-    assert system_major == required_major, f"This project requires Python {required_major}. Found: Python {sys.version}"
+    assert (
+        system_major == required_major
+    ), f"This project requires Python {required_major}. Found: Python {sys.version}"
